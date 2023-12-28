c425fe32f3b68de17072bd6e59252402d7a51c48
Pavel Chernyaev <pawliklist@yandex.ru>
2023-12-28 23:32:34 +0400

    optimization operations

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,7 +5,7 @@ fun main() {
     val minComission = 35.0
     val amount = 12590.0
 
-    var comission = (amount / 100 * comissionPercent * 100).roundToInt() / 100.0
+    var comission = (amount  * comissionPercent ).roundToInt() / 100.0
 
     if (comission < minComission) comission = minComission
 
